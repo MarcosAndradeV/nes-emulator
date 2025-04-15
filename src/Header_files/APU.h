018961cb980fb636cf66ebf9d76821474734ca22
DavidRyanFrancoGomes <davidryanfrancoo@gmail.com>
2025-04-15 02:24:33 -0300

    AI PAPAI

--- src/Header_files/APU.h
+++ src/Header_files/APU.h
@@ -1,6 +1,81 @@
 #pragma once
 
-class APU
-{
+#include <cstdint>
+#include <functional>
 
+class APU2A03{
+    public:
+        APU2A03();
+        ~APU2A03();
+        void clock();
+        void cpuWrite(uint16_t addr, uint8_t data);
+        uint8_t cpuRead(uint16_t addr);
+        void reset();
+        double GetOutputSample();
+
+
+    private:
+
+        uint32_t frame_clock_counter = 0;
+        uint32_t clock_counter = 0;
+        struct sequencer
+        {
+            uint32_t sequence = 0x000000000;
+            uint16_t timer = 0x0000;
+            uint16_t reload = 0x0000;
+            uint8_t output = 0x00;
+            uint8_t clock(bool bEnable, std::function<void(uint32_t &s)> funcManipulate)
+            {
+                if(bEnable)
+                {
+                    timer--;
+                    if(timer == 0xFFFF)
+                    {
+                        timer = reload+1;
+                        funcManipulate(sequence);
+                        output = sequence & 0x00000001;
+                    }
+                }
+                return output;
+            }
+        };
+
+    struct oscpulse
+	{
+		double frequency = 0;
+		double dutycycle = 0;
+		double amplitude = 1;
+		double pi = 3.14159;
+		double harmonics = 20;
+
+		double sample(double t)
+		{
+			double a = 0;
+			double b = 0;
+			double p = dutycycle * 2.0 * pi;
+
+            auto approxsin = [](double t)
+			{
+				double j = t * 0.15915;
+				j = j - (int)j;
+				return 20.785 * j * (j - 0.5) * (j - 1.0);
+			};
+
+			for (double n = 1; n < harmonics; n++)
+			{
+				double c = n * frequency * 2.0 * pi * t;
+				a += -approxsin(c) / n;
+				b += -approxsin(c - p * n) / n;
+			}
+
+			return (2.0 * amplitude / pi) * (a - b);
+		}
+	};
+
+        sequencer pulse01_seq;
+        oscpulse pulse01_osc;
+        bool pulse01_enabled = false;
+        double pulse01_sample = 0.0;
+        double dGlobalTime = 0.0;
 };
+
---
+++ src/Source_files/APU.cpp
@@ -0,0 +1,122 @@
+#include "APU.h"
+
+APU2A03::APU2A03()
+{
+}
+
+APU2A03::~APU2A03()
+{
+}
+
+void APU2A03::cpuWrite(uint16_t addr, uint8_t data){
+    switch(addr){
+        {
+            case 0x4000:
+                switch((data & 0xC0) >> 6)
+                {
+                    case 0x00: pulse01_seq.sequence = 0b00000001; break;
+                    case 0x01: pulse01_seq.sequence = 0b00000011; break;
+                    case 0x02: pulse01_seq.sequence = 0b00001111; break;
+                    case 0x03: pulse01_seq.sequence = 0b11111100; break;
+                }
+                break;
+            case 0x4001:
+                break;
+            case 0x4002:
+                pulse01_seq.reload = (pulse01_seq.reload & 0xFF00) | data;
+                break;
+            case 0x4003:
+                pulse01_seq.reload = (uint16_t)((data & 0x07)) << 8 | (pulse01_seq.reload & 0x00FF);
+                pulse01_seq.timer = pulse01_seq.reload;
+                break;
+            case 0x4004:
+                break;
+            case 0x4005:
+                break;
+            case 0x4006:
+                break;
+            case 0x4007:
+                break;
+            case 0x4008:
+                break;
+            case 0x400C:
+                break;
+            case 0x400E:
+                break;
+            case 0x4015:
+                pulse01_enabled = data & 0x01;
+                break;
+            case 0x400F:
+                break;
+        }
+
+uint8_t APU2A03::cpuRead(uint16_t addr)
+{
+    return 0x00;
+}
+
+void APU2A03::clock()
+{
+    bool bQuarterFrameClock = false;
+    bool bHalfFrameClock = false;
+
+    dGlobalTime += (0.3333333333 / 1789773.0);
+
+    if(clock_counter % 6 == 0)
+    {
+        frame_clock_counter++;
+
+        if (frame_clock_counter == 3729)
+		{
+			bQuarterFrameClock = true;
+		}
+
+		if (frame_clock_counter == 7457)
+		{
+			bQuarterFrameClock = true;
+			bHalfFrameClock = true;
+		}
+
+		if (frame_clock_counter == 11186)
+		{
+			bQuarterFrameClock = true;
+		}
+
+		if (frame_clock_counter == 14916)
+		{
+			bQuarterFrameClock = true;
+			bHalfFrameClock = true;
+			frame_clock_counter = 0;
+		}
+
+        if (bQuarterFrameClock)
+        {
+
+        }
+
+        if (bHalfFrameClock)
+        {
+
+        }
+
+        pulse01_seq.clock(pulse01_enabled, [](uint32_t &s){
+            s = ((s & 0x0001 << 7) | (s & 0x00FE) >> 1);
+        });
+
+        pulse01_osc.frequency = 1789773.0 / (16.0*(double)(pulse01_seq.reload+1));
+        pulse01_sample = pulse01_osc.sample(dGlobalTime);
+
+        clock_counter++;
+    }
+}
+
+void APU2A03::reset()
+{
+}
+
+double APU2A03::GetOutputSample()
+{
+    return pulse01_sample;
+}
+
+
