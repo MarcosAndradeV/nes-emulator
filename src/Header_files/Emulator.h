ef04eebc58aa797ca8da2c7bd99c107edc982915
tacio <moreiravini144@gmail.com>
2025-04-22 11:45:33 -0300

    Carregando jogos

--- src/Header_files/Emulator.h
+++ src/Header_files/Emulator.h
@@ -1,12 +1,19 @@
 #ifndef EMULATOR_H
 #define EMULATOR_H
 
+#include "../../include/global.h"
 
 /// Responsavel por inicialização, deinicialização e event loop.
 class Emulator {
 public:
     Emulator();
     ~Emulator();
+    
+    /// Carrega um jogo NES
+    /// @param path Caminho para o arquivo ROM
+    /// @return true se o jogo foi carregado com sucesso
+    bool loadGame(const std::string& path);
+    
     void run();
 };
 
